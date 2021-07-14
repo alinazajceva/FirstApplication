4e913d0defff62a2cd94ac5c2e90d1fcf334f53b
Alina <alinka_zajceva@list.ru>
2021-07-14 13:19:40 +0300

    script

--- script.js
+++ script.js
@@ -8,12 +8,20 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt ('Один из последних просмотренных фильмов?', ''),
-      b = prompt ('На сколько оцените его?', ''),
-      c = prompt ('Один из последних просмотренных фильмов?', ''),
-      d = prompt ('На сколько оцените его?', '');
+// const a = prompt ('Один из последних просмотренных фильмов?', ''),
+//       b = prompt ('На сколько оцените его?', ''),
+//       c = prompt ('Один из последних просмотренных фильмов?', ''),
+//       d = prompt ('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+
+for( let i = 0; i < 2; i++ ) {
+    const a = prompt ('Один из последних просмотренных фильмов?', ''),
+      b = prompt ('На сколько оцените его?', '');
+
+    personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
